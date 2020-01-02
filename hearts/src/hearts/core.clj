d8fd93ebaf3f47e841e12c63b5f0be026cfe037e
tye-shutty <shuttyt@gmail.com>
2020-01-02 13:26:15 -0400

    Work from laptop

--- hearts/src/hearts/core.clj
+++ hearts/src/hearts/core.clj
@@ -1,16 +1,10 @@
 (ns hearts.core)
 
-;;most of this can be put in a map and passed between functions, but atoms are good for troubleshooting and piecemeal development
-(def player-cards "player/dealer pos assoc with cards in sorted-set (dealer is 0, other decks start at 52) [#{8 15 42 46 51} #{1 7 33 43}]. aces high" (atom []))
-(def card-players "card pos assoc with player/dealer num [1 4]" (atom []))
 (def player-suits-broken "player pos then suit pos (y/n=1/0 for that suit) [[1 1 1 1] [0 1 1 0]]" (atom []))
-(def suit-players-broken "suit pos then player pos (y/n = 1/0) [[0 0 0 0] [0 1 1 0]]" (atom []))
-(def curr-winner "player and card" (atom [-1 -1]))
-(def hand-points "player pos [0 0 0 1]" (atom []))
+(def suit-players-broken "suit pos then player pos (y/n = 1/0) [[0 0 0 0 0] [0 1 1 0 0]]" (atom []))
 (def round-history "most recent hand first, most recent round first, sorted maps ordered by play, key is player, val is card '('({1 6 2 8 3 51 0 12}))" (atom '()))
-(def player-history "no purpose yet, doesn't have play order, just ordered by player then by round [[9 22] [5 16] [12 47] [0 13]]")
 
-(defn deal [] "todo: expand beyond 1 deck, 4 players, add humans."
+(defn deal [game-state] "todo: expand beyond 1 deck, 4 players, add humans."
   (let [numplayers 4]
     (loop [players (vec (take (inc numplayers) (repeat (sorted-set))))
            cards (vec (take 52 (repeat 0)))
@@ -19,24 +13,31 @@
       (if (< (count loose-cards) 1)
         (if (> (count needy-players) 0)
           (throw (Exception. "some players still need cards"))
-          {"player-cards" players
-           "card-players" cards
-           "pass-direction" -1 ;"-1=right, 1=left(clockwise, the order of the player-cards), 2=across(for now), 0=no passing"
-	   "points-history" '([0 0 0 0 0]) ;"hand pos (most recent to oldest) then player pos '([0 13 4 0 9] [0 0 13 13 0])"
-	   "points-total" [0 0 0 0 0]
-           "passed" [] ;player pos of most recent given cards
-           "human" [0 0 0 0 0]}) ;"player/dealer pos y/n [0 0 0 0 1]"
+          (conj game-state
+                {"player-cards" players ;"player/dealer pos assoc with cards in sorted-set (dealer is 0, other decks start at 52) [#{8 15 42 46 51} #{1 7 33 43}]. aces high"
+                 "card-players" cards ;"card pos assoc with player/dealer num [1 4]"
+                 "passed" [] ;player pos of this hand's passed cards [[4 '(0 51 40)] [1 '(37 38 12)]] ;source, cards
+                 "card-history" (cons '() (game-state "card-history")) ;"hand pos then round pos (most recent to oldest), then player pos, '(([-1 0 -1 -1 12]))"
+                 "first-player" (cons '() (game-state "first-player")) ;same structure as card-history to save order of play
+                 "points-history" (cons [0 0 0 0 0] (game-state "points-history"))}))
         (let [chosen-index (rand-int (count needy-players))
               [chosen chosen-count] (needy-players chosen-index)]
           (recur (update players chosen conj (first loose-cards))
-            (update cards (first loose-cards) (constantly chosen))
-            (rest loose-cards)
-            (if (> chosen-count 11)
-              (vec (keep-indexed #(if (= chosen-index %) nil %2) needy-players))
-              (update needy-players chosen-index #(list (first %) (inc (second %)))))))))))
+                 (update cards (first loose-cards) (constantly chosen))
+                 (rest loose-cards)
+                 (if (> chosen-count 11)
+                   (vec (keep-indexed #(if (= chosen-index %) nil %2) needy-players))
+                   (update needy-players chosen-index #(list (first %) (inc (second %)))))))))))
+
+(defn game-start []
+  (deal {"points-history" '() ;the accumulated points at the end of each hand, most recent first, then player pos '([0 13 4 0 9] [0 0 13 13 0])
+         "card-history" '()
+         "first-player" '()
+         "pass-direction" -1 ;"-1=right, 1=left(clockwise, the order of the player-cards), 2=across(for now), 0=no passing"
+         "human" [0 0 0 0 0]})) ;"player/dealer pos y/n [0 0 0 0 1]"
 
 (defn move-card [card from to game-state]
-  (prn "from" from "to" to "card" card)
+  #_(prn "from" from "to" to "card" card)
   (when (= from to) (throw (Exception. "a player is passing to itself")))
   (conj game-state
         {"player-cards" (let [z (update (game-state "player-cards") from
@@ -46,7 +47,7 @@
                           (update z to conj card))}
         {"card-players" (update (game-state "card-players") card (constantly to))}))
 
-(defn human-pass [human-player target-player]
+#_(defn human-pass [human-player target-player]
   (prn (str "Player " human-player ", what card of yours will you pass to player " target-player "?\n" "Your cards: " (@player-cards human-player)))
   (let [card (clojure.edn/read-string (read-line))] (if (= (type card) Long) (move-card card human-player target-player) (throw (Exception. "not a java.lang.Long")))))
 
@@ -59,42 +60,45 @@
     1 90))
 
 (defn pass [game-state]
-  (if (= (game-state "pass-direction") 0) nil
-    (update
+  (update
+   (if (= (game-state "pass-direction") 0)
+     game-state
      (let [count-player-cards (count (game-state "player-cards"))]
        (loop [player (- count-player-cards 1)
-              game-state game-state]
-         (if (< player 1) game-state
+              new-game-state game-state]
+         (if (< player 1) new-game-state
            (recur (- player 1)
-             (let [destination (case (+ player (game-state "pass-direction"))
-                                   count-player-cards 1
-                                   0 (- count-player-cards 1)
-                                   (+ player (game-state "pass-direction")))] ; edge cases
-                    (if (= ((game-state "human") player) 1)
-                      (human-select-pass player)
-                      (let [hand ((game-state "player-cards") player)
-                            suit-nums (reduce #(update %1 (int (/ %2 13)) inc)
-                                              [0 0 0 0]
-                                              hand) ;num cards in each suit
-                            suit-not-nums (mapv #(- 13 %) suit-nums) ; num cards not in each suit
-                            score (first (game-state "points-history"))
-                            special-card-weights {36 #_queen-spades (keep-queen player score)
-                                                  37 #_king-spades 100
-                                                  38 #_ace-spades 100
-                                                  0 #_two-clubs 18}
-                            suit-weights [1 1 1 2] ;should keep low hearts
-                            weights (map (fn [card] (vector (* (suit-weights (int (/ card 13)))
-                                                               (special-card-weights card card) ;hand weight is the value of the card if no special weight
-                                                               (suit-not-nums (int (/ card 13))))
-                                                            card))
-                                         hand)]
-                        #_(prn weights)
-                        (loop [top3 (take 3 (sort #(> (first %) (first %2)) weights)) ;[weight card]
-                               game-state game-state]
-                          (if (empty? top3) game-state
-                            (recur (rest top3)
-                                   (move-card (second (first top3)) player destination game-state)))))))))))
-     "pass-direction" #(case %, -1 1, 1 2, 2 0, 0 -1))))
+             (let [destination (condp = (+ player (game-state "pass-direction"));case doesn't work for some unknown reason
+                                 count-player-cards 1
+                                 0 (- count-player-cards 1)
+                                 (inc count-player-cards) 2
+                                 (+ player (game-state "pass-direction")))] ; edge cases
+               #_(prn count-player-cards player (game-state "pass-direction") destination)
+               (if (= ((game-state "human") player) 1)
+                 (human-select-pass player)
+                 (let [hand ((game-state "player-cards") player)
+                       suit-nums (reduce #(update %1 (int (/ %2 13)) inc)
+                                         [0 0 0 0]
+                                         hand) ;num cards in each suit
+                       suit-not-nums (mapv #(- 13 %) suit-nums) ; num cards not in each suit
+                       score (first (game-state "points-history"))
+                       special-card-weights {36 #_queen-spades (keep-queen player score)
+                                             37 #_king-spades 100
+                                             38 #_ace-spades 100
+                                             0 #_two-clubs 18}
+                       suit-weights [1 1 1 2] ;should keep low hearts
+                       weights (map (fn [card] (vector (* (suit-weights (int (/ card 13)))
+                                                          (special-card-weights card card) ;hand weight is the value of the card if no special weight
+                                                          (suit-not-nums (int (/ card 13))))
+                                                       card))
+                                    hand)]
+                   #_(prn weights)
+                   (loop [top3 (take 3 (sort #(> (first %) (first %2)) weights)) ;[weight card]
+                          new-game-state new-game-state]
+                     (if (empty? top3) new-game-state
+                       (recur (rest top3)
+                         (move-card (second (first top3)) player destination new-game-state))))))))))))
+     "pass-direction" #(condp = %, -1 1, 1 2, 2 0, 0 -1)))
 
 ;; makes low cards in suits that have been played/dealt more valuable ;in future can split into hand(player) and played(dealer) weights
 ;; just use 13 - card value for weights of broken suits, and 0.001 for suits that only I have
@@ -108,13 +112,14 @@
    ((game-state "player-cards2") (game-state "curr-player"))))
 
 ;;values to add to weights (highest will be chosen)
-;throw only happens if I'm not winning
+;throw only happens if I'm not able to win hand
+;additional logic is required elsewhere for shooting the moon
 (defn throw-weights [game-state]
   (let [curr-player (game-state "curr-player")]
     ((fn [hand]
-       (let [points-total (game-state "points-total")
+       (let [points-total (first (game-state "points-history"))
              winning (game-state "winning")
-             points-history (game-state "points-history")]
+             hand-points (apply mapv - (take 2 (concat (game-state "points-history") '([0 0 0 0 0]))))]
          (map #(cond ;no shoot the moon strategy for the queen of spades
                  (= % 36)
                  (if (or (and (not= (points-total curr-player) (apply min points-total)) ;not leading and
@@ -123,14 +128,12 @@
                               (< (- 13 (points-total curr-player)) (points-total (first winning))))) ;round winner is also losing
                    -1000 1000)
                  (or (= % 37) (= % 38))
-                 (if (reduce (fn [a v] (or a (= 36 (second v))))
-                             false
-                             (take-last (mod (count points-history) 4)))
+                 (if (((game-state "player-cards") 0) 36) ;36 has been played
                    0 750)
                  (> % 38)
-                 (+ % (cond (and (= 3 (count (filter zero? (first points-history)))) ;potential shoot moon
-                                 (not= (max (first points-history)) curr-player) ;I'm not shooting moon
-                                 (not= (max (first points-history)) ((first points-history) (first winning)))) ;round winner is not shooting moon
+                 (+ % (cond (and (< 1 (count (filter zero? points-total))) ;potential shoot moon
+                                 (not= (apply max hand-points) (hand-points curr-player)) ;I'm not shooting moon
+                                 (not= (apply max hand-points) (hand-points (first winning)))) ;round winner is not shooting moon
                         500
                         (and (not= curr-player (first winning)) ;about to lose
                              (> (points-total (first winning)) 95))
@@ -144,25 +147,19 @@
               hand)))
      ((game-state "player-cards2") curr-player))))
 
-(defn hand-init-state [game-state]
-  (let [fp ((game-state "card-players") 0)
-        game-state (move-card 0 fp 0 game-state)
-        player-cards2 (mapv #(vec (sort %)) (game-state "player-cards"))
+(defn choice-init [game-state]
+  (let [player-cards2 (mapv #(vec (sort %)) (game-state "player-cards"))
         player-suits (mapv (fn [hand] (reduce #(update %1 (int (/ %2 13)) inc)
                                               [0 0 0 0] hand))
                            player-cards2)]
     (conj game-state
-          {"first-player" fp
-           "curr-player" (let [np (+ fp 1)] (if (= np 5) 1 np))
-           "winning" [fp 0]  ; player, card
-           "hand-history" (list [fp 0])  ;newest at beginning
-           "broken" (concat (take 36 (repeat 1)) '(0 1 1) (take 13 (repeat 0)))  ; to be multiplied by weights
-           "player-cards2" player-cards2  ;for mapping across multiple weights of constant pos
+          {"player-cards2" player-cards2  ;for mapping across multiple weights of constant pos
            "player-suits" player-suits
            "suits-known" (mapv #(mapv + % (player-suits 0)) player-suits)})))
 
 ;; throw smaller weight card if no valid one
-(defn choice [game-state]
+;;returns card
+(defn subsequent-choice [game-state]
   (let [card-suit-rarity (card-suit-rarity game-state)
         throw-weights (throw-weights game-state)
         suit (int (/ (second (game-state "winning")) 13))]
@@ -175,8 +172,32 @@
                    (> (+ weight (throw-weights key)) (second choice)))  ;no matching suit yet and weight higher
               [card (+ weight (throw-weights key))]
               :else choice)))
-        (reduce-kv [-1 0] (mapv * card-suit-rarity (game-state "broken")))  ;card, weight ;broken stops first round point throws
+        (reduce-kv [-1 0] (mapv * card-suit-rarity (game-state "forbidden")))  ;card, weight ;forbidden stops first round point throws
         first)))
+
+(defn first-round-init [game-state]
+  (let [fp ((game-state "card-players") 0)
+        game-state (move-card 0 fp 0 game-state)]
+    (conj game-state
+          {"first-player" (cons (cons fp (first (game-state "first-player"))) (rest (game-state "first-player")))
+           "card-history" (cons (cons [-1 -1 -1 -1 -1] (first (game-state "card-history"))) (rest (game-state "card-history")))
+           "curr-player" (if (= fp 4) 1 (+ fp 1))
+           "forbidden" (concat (take 36 (repeat 1)) '(0 1 1) (take 13 (repeat 0)))  ; only for first round-history
+           "winning" [fp 0]}))) ;player, card
+
+(defn subsequent-round-init [game-state]
+  (let [fp (first (game-state "winning"))
+        choice (choice (choice-init game-state))]
+    (conj game-state
+          {"first-player" (cons (cons fp (first (game-state "first-player"))) (rest (game-state "first-player")))
+           "card-history" (cons (cons [-1 -1 -1 -1 -1] (first (game-state "card-history"))) (rest (game-state "card-history")))
+           "curr-player" (if (= fp 4) 1 (+ fp 1))
+           "winning" [fp choice]
+           "forbidden" })))
+
+(defn subsequent-play-card [game-state]
+  (conj game-state
+        {"curr-player" (if (= (game-state "curr-player") 4) 1 (inc (game-state "curr-player")))}))
 
 #_(defn play-hand [game-state]
   ;; loop for each player
--- hearts/test/hearts/core_test.clj
+++ hearts/test/hearts/core_test.clj
@@ -1,7 +1,124 @@
 (ns hearts.core-test
   (:require [clojure.test :refer :all]
-            [hearts.core :refer :all]))
+            [hearts.core :refer :all])
+  (:use [clojure.set :only [union intersection difference]]))
+;;test all with (clojure.test/run-tests 'hearts.core-test) or lein test
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest deal-test
+  (testing "Each player receives cards."
+    (is (= (count ((game-start) "player-cards"))
+           5))
+    (is (apply =
+               13
+               (map count (rest ((game-start) "player-cards")))))
+    (is (= (sort (apply concat (rest ((game-start) "player-cards"))))
+           (range 52)))
+    (is (= [0 13 13 13 13]
+           (reduce #(update % %2 inc)
+                   [0 0 0 0 0]
+                   ((game-start) "card-players")))))
+  (testing "Variables are initialized."
+    (is (= (sort '("player-cards", "card-players", "pass-direction", "passed",
+                   "card-history", "points-history", "human"))
+           (sort (keys (game-start)))))))
+
+(deftest pass-test
+  (let [pre-pass (game-start)
+        post-pass (pass pre-pass)
+        post-pass2 (pass post-pass)
+        post-pass3 (pass post-pass2)
+        post-pass4 (pass post-pass3)
+        post-pass5 (pass post-pass4)]
+    (testing "Each player passes 3 cards."
+             (is (= [0 10 10 10 10]
+                    (mapv (fn [old-hand new-hand]
+                            (count (filter (fn [old-card]
+                                             (new-hand old-card))
+                                           old-hand)))
+                          (pre-pass "player-cards")
+                          (post-pass "player-cards"))))
+             (is (= [0 10 10 10 10]
+                    (mapv (fn [old-hand new-hand]
+                            (count (filter (fn [old-card]
+                                             (new-hand old-card))
+                                           old-hand)))
+                          (post-pass "player-cards")
+                          (post-pass2 "player-cards")))))
+    (testing "Players pass in different directions."
+             (is (= [-1 4 1 2 3]
+                    (mapv (fn [source-hand]
+                            (reduce-kv (fn [acc key hand]
+                                            (if (= 3 (count (intersection hand source-hand)))
+                                              key acc))
+                                       -1
+                                       (post-pass "player-cards")))
+                          (pre-pass "player-cards"))))
+             (is (= [3 3 3 3]
+                    (let [diff (partition 2 (interleave (pre-pass "card-players") (post-pass "card-players")))]
+                      (vector (count (filter #(and (= 1 (first %)) (= 4 (second %))) diff))
+                              (count (filter #(and (= 2 (first %)) (= 1 (second %))) diff))
+                              (count (filter #(and (= 3 (first %)) (= 2 (second %))) diff))
+                              (count (filter #(and (= 4 (first %)) (= 3 (second %))) diff))))))
+             (is (= [-1 2 3 4 1]
+                    (mapv (fn [source-hand]
+                            (reduce-kv (fn [acc key hand]
+                                         (if (= 3 (count (intersection hand source-hand)))
+                                           key acc))
+                                       -1
+                                       (post-pass2 "player-cards")))
+                          (post-pass "player-cards"))))
+             (is (= [3 3 3 3]
+                    (let [diff (partition 2 (interleave (post-pass "card-players") (post-pass2 "card-players")))]
+                      (vector (count (filter #(and (= 1 (first %)) (= 2 (second %))) diff))
+                              (count (filter #(and (= 2 (first %)) (= 3 (second %))) diff))
+                              (count (filter #(and (= 3 (first %)) (= 4 (second %))) diff))
+                              (count (filter #(and (= 4 (first %)) (= 1 (second %))) diff))))))
+             (is (= [-1 3 4 1 2]
+                    (mapv (fn [source-hand]
+                            (reduce-kv (fn [acc key hand]
+                                         (if (= 3 (count (intersection hand source-hand)))
+                                           key acc))
+                                       -1
+                                       (post-pass3 "player-cards")))
+                          (post-pass2 "player-cards"))))
+             (is (= [3 3 3 3]
+                    (let [diff (partition 2 (interleave (post-pass2 "card-players") (post-pass3 "card-players")))]
+                      (vector (count (filter #(and (= 1 (first %)) (= 3 (second %))) diff))
+                              (count (filter #(and (= 2 (first %)) (= 4 (second %))) diff))
+                              (count (filter #(and (= 3 (first %)) (= 1 (second %))) diff))
+                              (count (filter #(and (= 4 (first %)) (= 2 (second %))) diff))))))
+             (is (= [-1 -1 -1 -1 -1]
+                    (mapv (fn [source-hand]
+                            (reduce-kv (fn [acc key hand]
+                                         (if (= 3 (count (intersection hand source-hand)))
+                                           key acc))
+                                       -1
+                                       (post-pass4 "player-cards")))
+                          (post-pass3 "player-cards"))))
+             (is (= [13 13 13 13]
+                    (let [diff (partition 2 (interleave (post-pass3 "card-players") (post-pass4 "card-players")))]
+                      (vector (count (filter #(and (= 1 (first %)) (= 1 (second %))) diff))
+                              (count (filter #(and (= 2 (first %)) (= 2 (second %))) diff))
+                              (count (filter #(and (= 3 (first %)) (= 3 (second %))) diff))
+                              (count (filter #(and (= 4 (first %)) (= 4 (second %))) diff))))))
+             (is (= [-1 4 1 2 3]
+                    (mapv (fn [source-hand]
+                            (reduce-kv (fn [acc key hand]
+                                         (if (= 3 (count (intersection hand source-hand)))
+                                           key acc))
+                                       -1
+                                       (post-pass5 "player-cards")))
+                          (post-pass4 "player-cards"))))
+             (is (= [3 3 3 3]
+                    (let [diff (partition 2 (interleave (post-pass4 "card-players") (post-pass5 "card-players")))]
+                      (vector (count (filter #(and (= 1 (first %)) (= 4 (second %))) diff))
+                              (count (filter #(and (= 2 (first %)) (= 1 (second %))) diff))
+                              (count (filter #(and (= 3 (first %)) (= 2 (second %))) diff))
+                              (count (filter #(and (= 4 (first %)) (= 3 (second %))) diff)))))))))
+
+(deftest first-round-init-test
+  (testing "Initialize information needed at the beginning of each hand."
+    (let [pre-init (pass (game-start))
+          post-init (first-round-init pre-init)]
+      (is (= #{"first-player", "curr-player", "winning", "broken"}
+             (difference (set (keys post-init)) (set (keys pre-init))))))))
